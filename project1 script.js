ade72ed506ffe69cfb49e2f96cc0898f53041488
abhayvsingh20@gmail.com <abhayvsingh20@gmail.com.com>
2022-12-13 21:58:00 +0530

    first commit

---
+++ project1 script.js
@@ -0,0 +1,16 @@
+const d= new Date();
+let hr= d.getHours();
+let min= d.getMinutes();
+let sec=d.getSeconds();
+ let hrota=
+ let minrota=
+ let secrota= 
+ document.getElementById("hour").style.transform:
+ document.getElementById("minute").style.transform:
+ document.getElementById("second").style.transform:
+
+ 
+
+console.log(hr);
+console.log(min);
+console.log(sec);
